bfa20f0b116bfc930482eb504648ae3d4d801143
Jonathan Si <jonathansi@g.ucla.edu>
2024-08-20 16:44:17 -0700

    mysql

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const router = require('./router/router')
 
 app.use(cors());
 app.use(express.json());
 
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "Jonathan",
+  password: "superkuber" //REMEMBER TO DOTENV THIS
+});
 
-
-
-
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 
 app.use((req, res) =>
---
+++ osp.session.sql
@@ -0,0 +1,10 @@
+-- @block
+
+CREATE TABLE tester(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name STRING,
+    location STRING,
+    bio STRING,
+    taxBracket INT
+);
+
