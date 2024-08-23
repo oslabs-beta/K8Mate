2015104b488c614ffcbd3f7b532fb8acefe94a17
User Name <ssinco90@gmail.com>
2024-08-22 17:07:43 -0700

    testflowtree

--- front-end/src/pages/Tree/Tree.jsx
+++ front-end/src/pages/Tree/Tree.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ReactFlow, Controls, Background } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
 function Tree() {
   return (
     <div>Tree</div>
   )
 }
+
 
 export default Tree
