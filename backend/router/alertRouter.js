cc80059d22c1847c8d7d1b4be971293d0c6da25f
Jonathan Si <jonathansi@g.ucla.edu>
2024-08-24 12:00:50 -0700

    get works

--- backend/router/alertRouter.js
+++ backend/router/alertRouter.js
@@ -3,10 +3,17 @@ const router = express.Router();
 
 const alertController = require('../controllers/alertController')
 
-router.post('/all',
+router.post('/create',
   alertController.createAlert,
   (req, res) => {
     return res.status(200).json(res.locals);
+  }
+)
+
+router.get('/all',
+  alertController.getAlert,
+  (req, res) => {
+    return res.status(200).json(res.locals.alerts);
   }
 )
 
