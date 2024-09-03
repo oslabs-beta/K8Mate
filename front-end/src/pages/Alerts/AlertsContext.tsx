0080c398edcb259daa6bf58503059e65f3e2ebf9
aaron <aaronthien052401@gmail.com>
2024-09-03 00:07:21 -0400

    updated alerts context error handling for failed fetch

--- front-end/src/pages/Alerts/AlertsContext.tsx
+++ front-end/src/pages/Alerts/AlertsContext.tsx
@@ -53,6 +53,10 @@ const AlertsProvider = ({ children }) => {
       try {
         const query = encodeURIComponent('node_memory_MemAvailable_bytes');
         const response = await fetch(`http://localhost:9090/api/v1/query?query=${query}`);
+        if (!response.ok){
+          console.log('Failed Fetch');
+          return
+        }
         const data = await response.json();
         // console.log("READ THIS SHIT HERE", data)
         const memData: AvailMem[] = data.data.result.map((mem) => {
@@ -62,6 +66,7 @@ const AlertsProvider = ({ children }) => {
           }
         });
         setAvailableMemory(memData)
+  
       } catch(err: unknown) {
         console.log(err)
       }
@@ -190,6 +195,10 @@ const AlertsProvider = ({ children }) => {
     const pods = async () => {
       try {
         const response = await fetch('http://localhost:9090/api/v1/query?query=kube_pod_info')
+        if (!response.ok){
+          console.log('Failed Fetch');
+          return
+        }
         const data = await response.json();
         // console.log('pod descriptions', data)
         const podData: PodDetails[] = data.data.result.map((pod) => {
@@ -249,8 +258,11 @@ const AlertsProvider = ({ children }) => {
       try {
         const query = encodeURIComponent('(irate(node_network_receive_bytes_total[1m]) + irate(node_network_transmit_bytes_total[1m]))');
         const response = await fetch(`http://localhost:9090/api/v1/query?query=${query}`);
+        if (!response.ok){
+          console.log('Failed Fetch');
+          return
+        }
         const data = await response.json();
-        
         if (data.status === 'success' && data.data.result.length > 0) {
           const trafficData: NetworkData[] = data.data.result.map(result => ({
             nodeName: result.metric.instance,
@@ -311,6 +323,10 @@ const AlertsProvider = ({ children }) => {
     const lowDiskSpace = async () => {
       try {
         const response = await fetch('http://localhost:9090/api/v1/query?query=100*(1-(node_filesystem_avail_bytes/node_filesystem_size_bytes))');
+        if (!response.ok){
+          console.log('Failed Fetch');
+          return
+        }
         const data = await response.json();
         console.log('DISK SPACE ALERT:', data);
         const diskData: DiskData[] = data.data.result.map((disk) => ({
@@ -369,6 +385,10 @@ const AlertsProvider = ({ children }) => {
     const cpuUsage = async () => {
       try {
         const response = await fetch('http://localhost:9090/api/v1/query?query=100 - (avg by (instance) (irate(node_cpu_seconds_total{mode="idle"}[10m]) * 100) * on(instance) group_left(nodename) (node_uname_info))');
+        if (!response.ok){
+          console.log('Failed Fetch');
+          return
+        }
         const data = await response.json();
         // console.log('DATA', data);
         const cpuUsagePerNode: CpuData[] = data.data.result.map((cpuUse) => ({
