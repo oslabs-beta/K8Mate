7abfd924544453b876bed7eef777439417055b05
Jonathan Si <jonathansi@g.ucla.edu>
2024-09-03 07:53:23 -0700

    commented out redundant js files

--- backend/controllers/alertController.js
+++ backend/controllers/alertController.js
@@ -1,67 +1,67 @@
-const db = require("../models/dbModel");
+// const db = require("../models/dbModel");
 
-const alertController = {};
+// const alertController = {};
 
-alertController.createAlert = (req, res, next) => {
-  const {id, name, log, category} = req.body;
-  // const dates = Date().split(' ').slice(0,5);
-  // const str = `${dates[0]} ${dates[1]} ${dates[2]} ${dates[3]} ${dates[4]}`;
-  // console.log(str);
-  const values = [category, id, name, log];
-  const qstring = `INSERT INTO alerts (category, node_id, node_name, log)
-                  VALUES ($1, $2, $3, $4)`;
-  db.query(qstring, values)
-    .then((data) => {
-      console.log('went through');
-      res.locals.alert = data;
-      return next();
-    })
-    .catch((err) => {
-      console.log('not working')
-      return next(err);
-    });
-}
+// alertController.createAlert = (req, res, next) => {
+//   const {id, name, log, category} = req.body;
+//   // const dates = Date().split(' ').slice(0,5);
+//   // const str = `${dates[0]} ${dates[1]} ${dates[2]} ${dates[3]} ${dates[4]}`;
+//   // console.log(str);
+//   const values = [category, id, name, log];
+//   const qstring = `INSERT INTO alerts (category, node_id, node_name, log)
+//                   VALUES ($1, $2, $3, $4)`;
+//   db.query(qstring, values)
+//     .then((data) => {
+//       console.log('went through');
+//       res.locals.alert = data;
+//       return next();
+//     })
+//     .catch((err) => {
+//       console.log('not working')
+//       return next(err);
+//     });
+// }
 
-alertController.getAlert = (req, res, next) => {
-  const qstring = `SELECT * FROM alerts`;
+// alertController.getAlert = (req, res, next) => {
+//   const qstring = `SELECT * FROM alerts`;
 
-  db.query(qstring)
-    .then((data) => {
-      res.locals.alerts = data.rows;
-      return next();
-    })
-    .catch((err) => {
-      return next(err);
-    })
-}
+//   db.query(qstring)
+//     .then((data) => {
+//       res.locals.alerts = data.rows;
+//       return next();
+//     })
+//     .catch((err) => {
+//       return next(err);
+//     })
+// }
 
-alertController.updateAlert = (req, res, next) => {
-  const {name, status, db_id} = req.body;
-  const values = [name, status, db_id]
-  const qstring = `UPDATE alerts SET read = $2 WHERE (node_name = $1 AND id = $3)`;
+// alertController.updateAlert = (req, res, next) => {
+//   const {name, status, db_id} = req.body;
+//   const values = [name, status, db_id]
+//   const qstring = `UPDATE alerts SET read = $2 WHERE (node_name = $1 AND id = $3)`;
 
-  db.query(qstring, values)
-    .then((data) => {
-      res.locals.alert = data;
-      return next();
-    })
-    .catch((err) => {
-      return next(err);
-    })
-}
+//   db.query(qstring, values)
+//     .then((data) => {
+//       res.locals.alert = data;
+//       return next();
+//     })
+//     .catch((err) => {
+//       return next(err);
+//     })
+// }
 
-alertController.deleteAlert = (req, res, next) => {
-  const {id, log} = req.body;
-  const values = [id, log]
-  const qstring = `DELETE FROM alerts WHERE (id = $1 AND log = $2)`;
+// alertController.deleteAlert = (req, res, next) => {
+//   const {id, log} = req.body;
+//   const values = [id, log]
+//   const qstring = `DELETE FROM alerts WHERE (id = $1 AND log = $2)`;
 
-  db.query(qstring, values)
-    .then((data) => {
-      res.locals.alert = data;
-      return next();
-    })
-    .catch((err) => {
-      return next(err);
-    })
-}
-module.exports = alertController;
+//   db.query(qstring, values)
+//     .then((data) => {
+//       res.locals.alert = data;
+//       return next();
+//     })
+//     .catch((err) => {
+//       return next(err);
+//     })
+// }
+// module.exports = alertController;
--- backend/router/alertRouter.js
+++ backend/router/alertRouter.js
@@ -1,35 +1,35 @@
-const express = require ('express');
-const router = express.Router();
+// const express = require ('express');
+// const router = express.Router();
 
-const alertController = require('../controllers/alertController')
+// const alertController = require('../controllers/alertController')
 
-router.post('/create',
-  alertController.createAlert,
-  (req, res) => {
-    return res.status(200).json(res.locals);
-  }
-)
+// router.post('/create',
+//   alertController.createAlert,
+//   (req, res) => {
+//     return res.status(200).json(res.locals);
+//   }
+// )
 
-router.get('/all',
-  alertController.getAlert,
-  (req, res) => {
-    return res.status(200).json(res.locals.alerts);
-  }
-)
+// router.get('/all',
+//   alertController.getAlert,
+//   (req, res) => {
+//     return res.status(200).json(res.locals.alerts);
+//   }
+// )
 
-router.put('/update',
-  alertController.updateAlert,
-  (req, res) => {
-    return res.status(200).json(res.locals);
-  }
-)
+// router.put('/update',
+//   alertController.updateAlert,
+//   (req, res) => {
+//     return res.status(200).json(res.locals);
+//   }
+// )
 
-router.delete('/delete',
-  alertController.deleteAlert,
-  (req, res) => {
-    return res.status(200).json(res.locals);
-  }
-)
+// router.delete('/delete',
+//   alertController.deleteAlert,
+//   (req, res) => {
+//     return res.status(200).json(res.locals);
+//   }
+// )
 
 
-module.exports = router;
+// module.exports = router;
