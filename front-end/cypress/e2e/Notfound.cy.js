9489db470600639279b5fdd8eaaef6eff75d5317
aaron <aaronthien052401@gmail.com>
2024-09-10 15:46:40 -0400

    edit to notfound cypress

--- front-end/cypress/e2e/Notfound.cy.js
+++ front-end/cypress/e2e/Notfound.cy.js
@@ -1,12 +1,20 @@
 /* global cy, it, describe */
 
-describe('should display a 404 error page if no route found', () => {
-    it('should navigate to the Alerts page and display the correct content', () => {
-        cy.visit('/*')
-        cy.get('[data-testid="titleName"]').should('exist')
-        .should('have.text', 'Super Kuber™')
-        cy.get('[data-testid="404"]').should('exist')
-        .should('have.text', '404 - Page Not Found')
-        cy.get('[data-testid="sorry"]').should('Sorry, the page you are looking for does not exist.')
-    })
-})
+describe('404 Error Page', () => {
+    it('should navigate to a non-existing page and display the correct content', () => {
+      cy.visit('/non-existing-page');
+      
+      cy.get('[data-testid="titleName"]')
+        .should('exist')
+        .and('have.text', 'Super Kuber™');
+      
+      cy.get('[data-testid="404"]')
+        .should('exist')
+        .and('have.text', '404 - Page Not Found');
+      
+      cy.get('[data-testid="sorry"]')
+        .should('exist')
+        .and('have.text', 'Sorry, the page you are looking for does not exist.');
+    });
+  });
+  
